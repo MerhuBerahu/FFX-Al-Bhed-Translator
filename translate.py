041b9b83e26d3ffb95ee3a08de3c426ff58fabf4
wmclaughlan <wmclaughlan@gmail.com>
2020-09-06 15:11:06 +0100

    adding colours

--- translate.py
+++ translate.py
@@ -2,11 +2,30 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-This module's deals Combat. Reading in the character, checking 
-their stats and that of the enemy, then doing the combat math
+This program will translate Final Fantasy X 'Al Bhed' into english
 """
 
-primer = {" ":" ","A":"E","B":"P","C":"S","D":"T","E":"I","F":"W","G":"K","H":"N","I":"U","J":"V","K":"G","L":"C","M":"L","N":"R","O":"Y","P":"B","Q":"X","R":"H","S":"M","T":"D","U":"O","V":"F","W":"Z","X":"Q","Y":"A","Z":"J"}
+
+from colorama import Fore, Back, Style, init
+init()
+
+#TODO
+# Add in errors, if letters no translatable
+# print out translated in pink
+# and errors print original input letters
+# as purple
+
+# Font Colors
+WHITE = Fore.WHITE
+RED = Fore.RED
+GREEN = Fore.GREEN
+YELLOW = Fore.YELLOW
+BLUE = Fore.BLUE
+MAGENTA = Fore.MAGENTA
+LIGHT_RED = Fore.LIGHTRED_EX
+CYAN = Fore.CYAN
+
+primer = {",":",","!":"!","'":"'","?":"?",".":"."," ":" ","A":"E","B":"P","C":"S","D":"T","E":"I","F":"W","G":"K","H":"N","I":"U","J":"V","K":"G","L":"C","M":"L","N":"R","O":"Y","P":"B","Q":"X","R":"H","S":"M","T":"D","U":"O","V":"F","W":"Z","X":"Q","Y":"A","Z":"J"}
 
 
 phrase = input("Type the al bhed phrase that you want to translate and hit 'Enter':-> ")
@@ -25,5 +44,5 @@ except:
     print("error")
         
 
-print("".join(str(x) for x in translation)) # Formatting to get rid of ' and ,
+print(MAGENTA,"".join(str(x) for x in translation)) # Formatting to get rid of ' and ,
 
